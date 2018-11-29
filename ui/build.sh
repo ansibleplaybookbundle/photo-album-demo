518aa072f7707069224a975b802f8be50fc26230
jkim <johnkim76@gmail.com>
2018-11-29 15:11:02 -0500

    fix security issues, and rebuild

--- ui/build.sh
+++ ui/build.sh
@@ -3,7 +3,8 @@
 set -x
 
 ng build --prod
-rm ././../demo-app/src/static/album/*
-cp -r dist/* ././../demo-app/src/static/album/
-rm ././../demo-app-s2i/album/static/album/app/*
-cp -r dist/* ././../demo-app-s2i/album/static/album/app/
+rm  -f ../app/src/static/album/*
+cp -r dist/* ../app/src/static/album/
+rm -f ../app-s2i/album/static/album/app/*
+cp -r dist/* ../app-s2i/album/static/album/app/
+
